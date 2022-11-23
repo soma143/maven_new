e2d229fe1bf94ace9190a6844fe67c6e597241e0
soma143 <somasanjana04@gmail.com>
2022-11-23 11:55:36 +0530

    Demo

---
+++ src/main/java/com/wa/sanjana/App.java
@@ -0,0 +1,13 @@
+package com.wa.sanjana;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is Se Lab Internal 1 Exam" );
+    }
+}
